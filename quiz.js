67eef1fa7ba60729bb2a62085d2e542f049f950e
SaiPriya01 <101970704+SaiPriya01@users.noreply.github.com>
2022-05-01 19:57:41 +0530

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,43 +1,66 @@
-var readlineSync=require("readline-sync");
-var score=0;
-var kQuestions=[{
+var readlineSync = require('readline-sync');
+var score = 0;
+var highScorers = [{
+    name: 'Maneesha',
+    score: 5
+  },
+  {
+    name: 'Bhargavi',
+    score: 4
+  }
+]
+var kQuestions = [{
   question: "which is the most viewed k-pop music video of all time? ",
   answer: "Gangnam Style"
-},{
+}, {
   question: "The non-korean idol in Blackpink  who is considered as 'Diamond-maknae'? ",
   answer: "Lisa"
-},{
+}, {
   question: "which is the most popular kpop group worldwide? ",
   answer: "BTS"
-},{
+}, {
   question: "Blackpink's jisoo made her acting debut in which kdrama? ",
   answer: "Snow drop"
-},{
+}, {
   question: "Who is considered as 'Golden-makanae' in BTS? ",
   answer: "Jungkook"
 }]
-function welcome() {
-  var userName=readlineSync.question("Please enter your name to start the quiz ");
-  console.log("Hey "+userName+"!! Welcome to Kpop Quiz");
+
+function welcomeUser() {
+  var userName = readlineSync.question('please enter your name: ');
+  console.log(`\nHi! ${userName}. Welcome to "KPOP-FANDOM-QUIZ" quiz`)
 }
-function Quiz(question,answer){
-  
-  var userAnswer=readlineSync.question(question);
-  if (userAnswer.toLowerCase()===answer.toLowerCase()){
-    console.log("Your answer is right");
-    score+=1;
-    console.log("-------------------------------");
+
+function playQuiz(question, answer) {
+  var userAnswer = readlineSync.question(question);
+  if (userAnswer.toLowerCase() === answer.toLowerCase()) {
+    score++;
+    console.log('you are right!');
+  } else {
+    console.log('you are wrong! \ncorrect answer is: ' + answer);
   }
-  else {
-    console.log("your answer is wrong");
-    console.log("-------------------------------");
-       }
+  console.log('current Score: ', score);
+  console.log('---------------------------------');
 }
-function play(){
-   welcome()
-  for (let i=0;i<kQuestions.length;i++){
-     Quiz(kQuestions[i].question,kQuestions[i].answer) 
+
+function askQuestion() {
+  for (let i = 0; i < kQuestions.length; i++) {
+    var currentQuestion = kQuestions[i];
+    playQuiz(currentQuestion.question, currentQuestion.answer);
   }
-  console.log("Yeah!! you have scored "+score+" points");
 }
-play();
+
+function showScores() {
+  if (score < 1) {
+    console.log('your final score is: ', score, '. Don\'t worry if you don\'t know me.Let\'s have a coffee someday!');
+  } else {
+    console.log('Hurrah! you have scored: ', score, 'you know me well');
+  }
+  console.log('---------------------------------');
+  console.log('\nBelow are the high scorers of this QUIZ. If you have scored more than them send me a screenshot, I will update it  here');
+  console.log('---------------------------------');
+  highScorers.map(names => console.log(`${names.name}: ${names.score}`));
+}
+welcomeUser();
+askQuestion();
+showScores();
